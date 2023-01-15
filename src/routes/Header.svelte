c769134e6cef8adf17a7ae935dd27f7c5e1e83a0
Hugos68 <hugokorteapple@gmail.com>
2023-01-16 00:44:30 +0100

    Adjusted hamburger animation to use pixel values for precision

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -13,13 +13,15 @@
 </script>
 
 <AppBar class="h-[var(--header-height)]">
-	<svelte:fragment slot="lead"><h2>Subjective</h2></svelte:fragment>
+	<svelte:fragment slot="lead">
+        <a href="/home" class="font-bold text-2xl">Subjective</a>
+    </svelte:fragment>
 	<svelte:fragment slot="trail">
         <button class="btn p-0 md:hidden " on:click={() => openHamburger()}>
-            <svg class="w-8 h-8" viewBox="0 0 100 100">
-                <rect fill="currentColor" width="75" height="10" x="25" y="15"></rect>
-                <rect fill="currentColor" width="65" height="10" x="35" y="45"></rect>
-                <rect fill="currentColor" width="90" height="10" x="10" y="75"></rect>
+            <svg class="w-8 h-8 group" viewBox="0 0 100 100">
+                <rect class="group-hover:translate-x-[15px] transition-transform duration-300" fill="currentColor" width="75" height="10" x="25" y="15"></rect>
+                <rect class="group-hover:translate-x-[-20px] transition-transform duration-300" fill="currentColor" width="85" height="10" x="35" y="45"></rect>
+                <rect class="group-hover:translate-x-[10px] transition-transform duration-300" fill="currentColor" width="90" height="10" x="10" y="75"></rect>
             </svg>
         </button>
         <nav class="hidden md:block">
