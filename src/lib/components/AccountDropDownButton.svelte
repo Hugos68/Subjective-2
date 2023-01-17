8825a19d9455d940528889910cdb3d3a0ea143bf
hugos68 <hugokorteapple@gmail.com>
2023-01-18 00:00:00 +0100

    Changed some mini values for better button hitbox

--- src/lib/components/AccountDropDownButton.svelte
+++ src/lib/components/AccountDropDownButton.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <span class="relative">
-	<button class="btn" use:menu={{ menu: 'account-drop-down-menu' }} on:click={() => console.log("click")}>
+	<button class="btn btn-sm" use:menu={{ menu: 'account-drop-down-menu' }} on:click={() => console.log("click")}>
         <svg class="h-6 w-6 text-token" fill="currentColor" viewBox="0 0 32 32"><path d="M16,16A7,7,0,1,0,9,9,7,7,0,0,0,16,16ZM16,4a5,5,0,1,1-5,5A5,5,0,0,1,16,4Z"/><path d="M17,18H15A11,11,0,0,0,4,29a1,1,0,0,0,1,1H27a1,1,0,0,0,1-1A11,11,0,0,0,17,18ZM6.06,28A9,9,0,0,1,15,20h2a9,9,0,0,1,8.94,8Z"/></svg>
     </button>
 	<nav class="bg-surface-200-700-token p-4 rounded-token list-nav" data-menu="account-drop-down-menu">
