fcd19446f1387a9eca4f1f3f089b1d614567a548
hugos68 <hugokorteapple@gmail.com>
2023-01-18 19:34:24 +0100

    Nicer styling and new theme

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -40,7 +40,7 @@
                 <button class="btn bg-primary-400-500-token" on:click={() => openConnect()}>Connect</button>
             {:else}
                 <span class="relative">
-                    <button class="btn btn-sm" use:menu={{ menu: 'account-drop-down-menu-header' }}>
+                    <button class="btn hover:card-glass-primary" use:menu={{ menu: 'account-drop-down-menu-header' }}>
                         <svg class="h-6 w-6 text-token" fill="currentColor" viewBox="0 0 32 32"><path d="M16,16A7,7,0,1,0,9,9,7,7,0,0,0,16,16ZM16,4a5,5,0,1,1-5,5A5,5,0,0,1,16,4Z"/><path d="M17,18H15A11,11,0,0,0,4,29a1,1,0,0,0,1,1H27a1,1,0,0,0,1-1A11,11,0,0,0,17,18ZM6.06,28A9,9,0,0,1,15,20h2a9,9,0,0,1,8.94,8Z"/></svg>
                     </button>
                     <nav class="bg-surface-200-700-token shadow-xl p-4 rounded-token list-nav" data-menu="account-drop-down-menu-header">
