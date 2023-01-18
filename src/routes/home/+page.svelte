f3a68ce68f66ac5d64c112b9ebb4ab69dbc18a3c
hugos68 <hugokorteapple@gmail.com>
2023-01-18 20:07:45 +0100

    Weird jumpy pixel glitch

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,6 +1,10 @@
+<script lang="ts">
+	import { fly } from "svelte/transition";
+</script>
+
 <div class="mt-[12.5vh]">
-    <h1 class="text-[clamp(3.25rem,15vw,10rem)] leading-snug flex flex-col">
+    <h1 class="text-end text-[clamp(3.25rem,15vw,10rem)] leading-snug flex flex-col" in:fly={{x:-250, duration: 750}}>
         <span>Welcome to</span>
-        <span class="gradient-heading">Subjective</span>
+        <span class="gradient-heading" in:fly={{delay: 100, x: -250, duration: 1000}}>Subjective</span>
     </h1>
 </div>
