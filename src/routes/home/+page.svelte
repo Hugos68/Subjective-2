f8e18ae52413865afed8c651a7bee960b2c7d754
hugos68 <hugokorteapple@gmail.com>
2023-01-21 00:55:50 +0100

    Responsiveness for home page

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -3,13 +3,13 @@
     import {cubicInOut} from "svelte/easing";
 </script>
 
-<main class="flex justify-evenly flex-col md:flex-row gap-12 md:gap-0  items-center mt-[25vh]">
-    <h1 class="text-center text-[clamp(3.25rem,15vw,10rem)] md:text-[clamp(3.25rem,10vw,10rem)] leading-snug flex flex-col" in:fly={{x:-250, duration: 750, easing: cubicInOut}}>
+<main class="flex justify-evenly flex-col lg:flex-row gap-12 lg:gap-0  items-center mt-[15vh]">
+    <h1 class="text-center text-[clamp(3.25rem,11vw,10rem)] leading-snug flex flex-col" in:fly={{x:-250, duration: 750, easing: cubicInOut}}>
         <span class="break-normal">Welcome to</span>
         <span class="gradient-heading" in:fly={{delay: 100, x: -250, duration: 1000}}>Subjective</span>
     </h1>
-    <figure class="relative w-[clamp(15rem,30vw,30rem)] aspect-square" in:fade={{delay: 100, duration: 1000}}>
-        <section class="w-[clamp(15rem,30vw,30rem)] aspect-square rounded-full absolute bg-gradient-to-r from-secondary-500 to-primary-500 blur-[128px]"></section>
+    <figure class="relative w-[clamp(15rem,35vw,30rem)] aspect-square" in:fade={{delay: 100, duration: 1000}}>
+        <section class="w-[clamp(20rem,35vw,30rem)] aspect-square rounded-full absolute bg-gradient-to-r from-secondary-500 to-primary-500 blur-[128px]"></section>
         <lottie-player src="https://assets6.lottiefiles.com/private_files/lf30_esg1l8r1.json"  speed="0.25" loop  autoplay></lottie-player>
     </figure>
 </main>
