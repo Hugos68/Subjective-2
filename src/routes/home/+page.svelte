553aab7ac3f5acde5c788bbff2c90a5760e22e36
hugos68 <hugokorteapple@gmail.com>
2023-01-21 01:08:46 +0100

    Added custom pulsing animation to the background

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -9,7 +9,7 @@
         <span class="gradient-heading" in:fly={{delay: 100, x: -250, duration: 1000}}>Subjective</span>
     </h1>
     <figure class="relative w-[clamp(20rem,35vw,35rem)] aspect-square" in:fade={{delay: 100, duration: 1000}}>
-        <section class="w-[clamp(20rem,35vw,35rem)] aspect-square rounded-full absolute bg-gradient-to-r from-secondary-500 to-primary-500 blur-[128px]"></section>
+        <section class="w-[clamp(20rem,35vw,35rem)] aspect-square rounded-full absolute bg-gradient-to-r from-secondary-500 to-primary-500 blur-[96px] custom-pulse"></section>
         <lottie-player src="https://assets6.lottiefiles.com/private_files/lf30_esg1l8r1.json"  speed="0.25" loop  autoplay></lottie-player>
     </figure>
 </main>
