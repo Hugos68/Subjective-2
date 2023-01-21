e2dc94081e2911753aa6ee0e63b29c04a1814cea
Hugos68 <hugokorteapple@gmail.com>
2023-01-21 02:40:49 +0100

    Nice little text

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -4,10 +4,16 @@
 </script>
 
 <main class="flex justify-evenly flex-col lg:flex-row gap-12 lg:gap-0 items-center">
-    <h1 class="text-center text-[clamp(3.25rem,11vw,10rem)] font-bold drop-shadow-2xl leading-snug flex flex-col" in:fly={{x:-250, duration: 750, easing: cubicInOut}}>
-        Welcome to
-        <span class="gradient-heading" in:fly={{delay: 100, x: -250, duration: 1000}}>Subjective</span>
-    </h1>
+    <div class="flex flex-col items-center gap-10">
+        <h1 class="text-center text-[clamp(3.25rem,15vw,8rem)] font-bold drop-shadow-2xl leading-snug flex flex-col" in:fly={{x:-250, duration: 750, easing: cubicInOut}}>
+            <span class="whitespace-nowrap">Welcome to</span>
+            <span class="gradient-heading" in:fly={{delay: 100, x: -250, duration: 1000}}>Subjective</span>
+        </h1>
+        <p class="text-center font-semibold max-w-[65ch]" in:fade={{delay: 500, duration: 1000}}>
+            Subjective provides some of the best quality clothing for the lowest price possible, we accomplish this by taking a different approach in this hot industry, stop wanting difference, BE the difference.
+        </p>
+    </div>
+
     <figure class="relative w-[clamp(20rem,35vw,35rem)] aspect-square" in:fade={{delay: 100, duration: 1000}}>
         <section class="w-[clamp(20rem,35vw,35rem)] aspect-square rounded-full absolute bg-gradient-to-r from-secondary-500 to-primary-500 blur-[96px] custom-pulse"></section>
         <lottie-player src="https://assets6.lottiefiles.com/private_files/lf30_esg1l8r1.json"  speed="0.25" loop  autoplay></lottie-player>
