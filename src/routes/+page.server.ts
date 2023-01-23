89280020528c43a4d2d30e7de7de244944845594
Hugos68 <hugokorteapple@gmail.com>
2023-01-23 11:49:52 +0100

    added cookie liftetime

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -104,7 +104,7 @@ export const actions = ({
     setCookieConsent: async ({cookies, url}) => {
         const serializedCookieConsent: string | null = url.searchParams.get('cookie');
         if (!serializedCookieConsent) return fail(400, {message: "Action requires cookie of type 'ConsentCookie'"});
-        cookies.set('consent', serializedCookieConsent, { path: '/', expires: 60 * 60 * 24 * 365 * 10});
+        cookies.set('consent', serializedCookieConsent, { path: '/', maxAge: 60 * 60 * 24 * 365 * 10});
         return {
             success: true
         }
